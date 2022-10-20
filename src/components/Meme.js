0c7204493746e3a67d80b3f039cd736911108e73
baranero <jakub.baran2@gmail.com>
2022-10-20 06:30:43 +0200

    Added letters to the image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -9,28 +9,60 @@ export default function Meme() {
 
     const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
-    function generateMeme() {
+    function generateMeme(event) {
         const memesArray = allMemeImages.data.memes
         let randomNumber = Math.floor(Math.random()*memesArray.length)
         console.log(memesArray[randomNumber].url);
+        console.log(event.target.value);
         setMeme(prevState => {
             return {
             ...prevState,
-            randomImage: memesArray[randomNumber].url}})
+            randomImage: memesArray[randomNumber].url,
+            [event.target.name]: event.target.value,
+        }})
         }
 
-
+    function handleSubmit(event) {
+        event.preventDefault()
+        console.log(meme);
+    }
     
     return (
-        <main className="input-container">
-            <div className="input-text">
-                <input placeholder="Top of meme" className="input-input"/>
-                <input placeholder="Bottom of meme" className="input-input"/>
-                <button onClick={generateMeme} className="input-generate-button">Get a new meme image</button>
-            </div>
-            <div className="meme-cont">
-                <img src={meme.randomImage} className="meme-img"/>
-            </div>
+        <main className="input-container" >
+            <form onSubmit={handleSubmit}>
+                <div className="input-text">
+                    <input
+                        name="topText"
+                        value={setMeme.topText}
+                        placeholder="Top of meme"
+                        className="input-input"
+                        onChange={generateMeme}
+                    />
+                    <input
+                        name="bottomText"
+                        value={setMeme.bottomText}
+                        placeholder="Bottom of meme"
+                        className="input-input"
+                        onChange={generateMeme}
+                    />
+                    <button
+                        onClick={generateMeme}
+                        className="input-generate-button">
+                        Get a new meme image
+                    </button>
+                </div>
+                <div className="meme-cont">
+                    <img
+                        onChange={generateMeme}
+                        className="meme-img"
+                        name="randomImage"
+                        value={meme.randomImage}
+                        src={meme.randomImage}
+                    />
+                    <h2 className="meme-text top"></h2>
+                    <h2 className="meme-text bottom"></h2>
+                </div>
+            </form>
         </main>
     )
 }
